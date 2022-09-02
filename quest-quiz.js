ae33027cf9bf19c86456074f570e9655935758fe
subhanrizwan <subhanahmed444sherazi@gmail.com>
2022-09-02 08:13:42 +0500

    quiz-app

---
+++ quest-quiz.js
@@ -0,0 +1,228 @@
+function join(){
+  var main = document.getElementById("main-box")
+  main.style.display="block"
+  var main1 = document.getElementById("misbah")
+  main1.style.display="none"
+  }
+  
+  function javascript(){
+  var ps = document.getElementById("quiz-app")
+  ps.style.display="block"
+  var js = document.getElementById("main-box")
+  js.style.display="none"
+  }
+
+  var score=0;
+
+  function nextquest(){
+    var ans_1 = document.getElementById('correct-ans1')
+if(ans_1.checked){
+  score++;
+  console.log(score);
+var seccard=document.getElementById('sec-card')
+seccard.style.display="block"
+
+var firstcard = document.getElementById('first-card')
+firstcard.style.display="none"
+}else{alert("please ans the question")}
+
+  }
+
+function nextquest2(){
+  var ans_2 = document.getElementById('correct-ans2')
+
+  if(ans_2.checked){
+    score++;
+     console.log(score);
+
+     var seccard=document.getElementById('third-card')
+     seccard.style.display="block"
+
+     var firstcard = document.getElementById('sec-card')
+     firstcard.style.display="none"
+  }else{alert("please ans the question")}
+} 
+
+function nextquest3(){
+  var ans_3 = document.getElementById('correct-ans3')
+  if(ans_3.checked){
+    score++;
+    console.log(score);
+
+     var seccard=document.getElementById('fourth-card')
+     seccard.style.display="block"
+ 
+     var firstcard = document.getElementById('third-card')
+     firstcard.style.display="none"
+ } else{alert("please ans the question")}
+  
+} 
+
+function submit(){
+  var ans_4 = document.getElementById('correct-ans4')
+  if(ans_4.checked){
+    score++;
+    console.log(score);
+
+     var res=document.getElementById('result-box')
+     res.style.display="block"
+ 
+     var firstcard = document.getElementById('fourth-card')
+     firstcard.style.display="none"
+ } else{alert("please ans the question")}
+}
+
+
+
+// var allquestions = [
+// {
+// question : "hello",
+// answer :  "han",
+// selects:[
+//     "subhan",
+//     "ahmed",
+//     "subham",
+//     "ahmed"
+//   ]
+
+// },
+// {
+//     question : "hel",
+// answer :  "han",
+// selects:[
+//     "ahmed",
+//     "subhan",
+//     "ahmed",
+//     "subham",
+//   ]
+// },
+// {
+//     question : "hello",
+// answer :  "han",
+// selects:[
+//     "subham",
+//     "subhan",
+//     "ahmed",
+//     "aaaaaa"
+//   ]
+// }
+// ]
+
+// function  showQues(e){
+//     var quest = document.getElementById("showquestion");
+//     quest.innerHTML = allquestions[e].question;
+
+//     // show answer
+
+//     var ans = document.getElementsByClassName("opt");
+// for(var i=0; i < ans.length; i++){
+// ans[i].innerHTML = allquestions[e].selects[i]
+// }
+// }
+
+// var count = 0;
+
+// function nextbtn(){
+// count++;
+// showQues(count);
+// }
+// const quizData = [
+//   {
+//       question: "Which language runs in a web browser?",
+//       a: "Java",
+//       b: "C",
+//       c: "Python",
+//       d: "javascript",
+//       correct: "d",
+//   },
+//   {
+//       question: "What does CSS stand for?",
+//       a: "Central Style Sheets",
+//       b: "Cascading Style Sheets",
+//       c: "Cascading Simple Sheets",
+//       d: "Cars SUVs Sailboats",
+//       correct: "b",
+//   },
+//   {
+//       question: "What does HTML stand for?",
+//       a: "Hypertext Markup Language",
+//       b: "Hypertext Markdown Language",
+//       c: "Hyperloop Machine Language",
+//       d: "Helicopters Terminals Motorboats Lamborginis",
+//       correct: "a",
+//   },
+//   {
+//       question: "What year was JavaScript launched?",
+//       a: "1996",
+//       b: "1995",
+//       c: "1994",
+//       d: "none of the above",
+//       correct: "b",
+//   },
+
+
+// ];
+
+// const quiz= document.getElementById('quiz-cont')
+// const answerEls = document.querySelectorAll('.ans')
+// const questionEl = document.getElementById('question')
+// const a_text = document.getElementById('a_text')
+// const b_text = document.getElementById('b_text')
+// const c_text = document.getElementById('c_text')
+// const d_text = document.getElementById('d_text')
+// const submitBtn = document.getElementById('submit')
+
+
+// let currentQuiz = 0
+// let score = 0
+
+// loadQuiz()
+
+// function loadQuiz() {
+
+//   deselectAnswers()
+
+//   const currentQuizData = quizData[currentQuiz]
+
+//   questionEl.innerText = currentQuizData.question
+//   a_text.innerText = currentQuizData.a
+//   b_text.innerText = currentQuizData.b
+//   c_text.innerText = currentQuizData.c
+//   d_text.innerText = currentQuizData.d
+// }
+
+// function deselectAnswers() {
+//   answerEls.forEach(answerEl => answerEl.checked = false)
+// }
+
+// function getSelected() {
+//   let answer
+//   answerEls.forEach(answerEl => {
+//       if(answerEl.checked) {
+//           answer = answerEl.id
+//       }
+//   })
+//   return answer
+// }
+
+
+// submitBtn.addEventListener('click', () => {
+//   const answer = getSelected()
+//   if(answer) {
+//      if(answer === quizData[currentQuiz].correct) {
+//          score++
+//      }
+
+//      currentQuiz++
+
+//      if(currentQuiz < quizData.length) {
+//          loadQuiz()
+//      } else {
+//          quiz.innerHTML = `
+//          <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+//          <button onclick="location.reload()">Reload</button>
+//          `
+//      }
+//   }
+// })
---
+++ quiz-2.js
@@ -0,0 +1,53 @@
+
+// var btn = document.getElementById('creatbtn')
+function final_login(){
+    event.preventDefault();
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+
+    var email1 = localStorage.getItem("email")
+    var pass1 = localStorage.getItem("pass")
+    if(email1 === email && password === pass1){
+
+        window.location = "cousres.html"
+    }else{alert("please enter the email,phone,name" )}
+
+
+}
+
+function creat_acc(){
+    event.preventDefault();
+    window.location ="creatacc.html"
+    // btn.remove
+}
+function login(){
+
+    event.preventDefault();
+  
+    var full_firstname= document.getElementById('first_reg').value
+    var full_lastname = document.getElementById('last_reg').value
+    var full_email = document.getElementById('email_reg').value
+    var full_pass = document.getElementById('pass_reg').value
+    
+    localStorage.setItem('firstname'  , full_firstname)
+    localStorage.setItem('lastname' , full_lastname )
+    localStorage.setItem('email'  , full_email)
+    localStorage.setItem('pass' , full_pass)
+    
+    var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(full_email)
+    var pass = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(full_pass)
+   
+    if( email){
+     if(pass){
+     }
+      window.location = 'quiz-login.html'
+    }else{alert("email is required")}
+  }
+  
+
+ 
+
+ 
+
+
